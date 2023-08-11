affc2e5503f646374dc26c0d50489099f10560e0
prajwal108 <prajwalk8976@gmail.com>
2023-08-11 12:23:29 +0530

    form submission setup

--- js/form.js
+++ js/form.js
@@ -11,15 +11,17 @@
                 dataType: "json",
                 success: function(response) {
                     // Handle success, e.g., display a thank you message
-                    alert("Thank you for your submission!");
-                    // You can customize this message or use a different method to display it.
+                    $("#contact-form").hide();
+                    $("#success-message").show();
                 },
                 error: function(response) {
                     // Handle error, e.g., display an error message
                     alert("An error occurred. Please try again later.");
                     // You can customize this message or use a different method to display it.
                 }
+                
             });
+            
         });
     });
 
